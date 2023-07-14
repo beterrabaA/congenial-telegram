208f527406ccc09136f0abeb393d4251e0954db5
Lucas Vieira <ls6182315@gmail.com>
2023-07-14 01:06:39 +0000

    css

---
+++ components/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+    return (
+        <div className="search-bar border-black border-radius-full m-4 border-8">
+        <input type="text" placeholder="Pesquisa contatos" />
+        </div>
+    );
+}
+
+export default SearchBar;
